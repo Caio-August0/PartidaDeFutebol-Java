4b7048412361aee29cc87645689f8840846eef9f
Caio Augusto <caioaugustobr0106@gmail.com>
2022-08-06 18:48:38 -0300

    "first commit"

---
+++ src/domian/Jogador.java
@@ -0,0 +1,26 @@
+package domian;
+
+import java.util.Date;
+
+public class Jogador {
+
+    private int id;
+    private int numeroDaCamisa;
+    private int qualidade;
+    private int cartoes;
+
+    private String nome;
+    private String apelido;
+    private String posicao;
+
+    private boolean suspenso;
+
+    private Date dataDeNascimento;
+
+    public Jogador() {
+
+    }
+
+
+
+}
---
+++ src/test/ClassePrincipal.java
@@ -0,0 +1,10 @@
+package test;
+
+import domian.Jogador;
+
+public class ClassePrincipal {
+    public static void main(String[] args) {
+        
+
+    }
+}
